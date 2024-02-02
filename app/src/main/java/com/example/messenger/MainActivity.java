9eae6552451eaff695e4a12666c9052460580d25
Yrka <varnack30@gmail.com>
2024-02-02 20:15:34 +0300

    Сделал чат избранное

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         //Инициализация переменных для элементов разметки
         chatsList = findViewById(R.id.chats_list_layout);
+
+        //Заполнение списка чатов(пока что тут только избранное)
+        ChatPreviewFragment favourites = new ChatPreviewFragment(R.drawable.favourites, "Избранное");
+        FragmentTransaction chatsListFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        chatsListFragmentTransaction.add(chatsList.getId(), favourites);
+        chatsListFragmentTransaction.commit();
 
     }
 }
