68a47724a48270a9e8f33e0a309981bf4ea37edc
Yrka <varnack30@gmail.com>
2024-02-03 15:35:32 +0300

    Сделал возможность отправки сообщения пользователем

--- app/src/main/java/com/example/messenger/ChatActivity.java
+++ app/src/main/java/com/example/messenger/ChatActivity.java
@@ -3,16 +3,24 @@ package com.example.messenger;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ChatActivity extends AppCompatActivity {
 
     //Создание переменных для эл. разметки
     FrameLayout chatHeaderContainer;
     LinearLayout messagesList;
+    Button sendMsg;
+    EditText userText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,8 @@ public class ChatActivity extends AppCompatActivity {
         //Инициализация переменных для эл. разметки
         chatHeaderContainer = findViewById(R.id.chat_header_container);
         messagesList = findViewById(R.id.messages_list);
+        sendMsg = findViewById(R.id.send_message_btn);
+        userText = findViewById(R.id.user_text_et);
 
         //Получение названия и иконки чата
         String chatName = getIntent().getStringExtra("chat_name");
@@ -33,20 +43,47 @@ public class ChatActivity extends AppCompatActivity {
         //Создание заголовка чата
         ChatHeaderFragment header = new ChatHeaderFragment(chatLogoId, chatName);
 
-        //Инициализация транзакции для вставки фрагментов в активити
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-
         //Помещение заголовка в контейнер
-        fragmentTransaction.add(chatHeaderContainer.getId(), header);
+        FragmentTransaction headTransaction = getSupportFragmentManager().beginTransaction();
+        headTransaction.add(chatHeaderContainer.getId(), header);
+        headTransaction.commit();
 
         //Заполнение уже существующими сообщениями
         for(int i = 0; i < sharedPreferences.getInt(chatName+"-items", 0); i++){
-            MessageFragment msg = new MessageFragment(sharedPreferences.getString(chatName+"msg"+i, null));
-            if(msg!=null)
-                fragmentTransaction.add(messagesList.getId(),msg);
+            String msg = sharedPreferences.getString(chatName+"-msg-"+i, null);
+            if(msg!=null){
+                FragmentTransaction msgTransaction = getSupportFragmentManager().beginTransaction();
+                msgTransaction.add(messagesList.getId(), new MessageFragment(msg));
+                msgTransaction.commit();
+            }
+
         }
 
-        //Коммит всех изменений
-        fragmentTransaction.commit();
+        //Создание нового сообщения
+        sendMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userText.length() > 0){
+                    //Отображение нового сообщения
+                    MessageFragment newMsg = new MessageFragment(userText.getText().toString());
+                    FragmentTransaction newMsgTransaction = getSupportFragmentManager().beginTransaction();
+                    newMsgTransaction.add(messagesList.getId(), newMsg);
+                    newMsgTransaction.commit();
+                    //Это код, чтобы убрать клаву
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    //Сохранение сообщения в памяти
+                    int item = sharedPreferences.getInt(chatName+"-items", 0);
+                    sharedPreferences.edit().putString(chatName+"-msg-"+item, userText.getText().toString()).apply();
+                    item+=1;
+                    sharedPreferences.edit().putInt(chatName+"-items", item).apply();
+                    //Сбрасывание пользовательского текста
+                    userText.setText("");
+
+                }
+
+            }
+        });
+
     }
 }
